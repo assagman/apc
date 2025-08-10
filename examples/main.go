5ba88e0a64078f0b6da032979c1d301381a41d5c
assagman <ahmetsercansagman@gmail.com>
2025-08-10 15:37:38 +0300

    simplify example main
    Signed-off-by: assagman <ahmetsercansagman@gmail.com>

--- examples/main.go
+++ examples/main.go
@@ -6,122 +6,48 @@ import (
 	"fmt"
 
 	"github.com/assagman/apc"
+	// "github.com/assagman/apc/internal/providers"
 	// "github.com/assagman/apc/internal/http"
 )
 
+var providerConfig = map[string]string{
+	"openrouter": "z-ai/glm-4.5",
+	"groq":       "moonshotai/kimi-k2-instruct",
+	"cerebras":   "gpt-oss-120b",
+	"openai":     "gpt-5-nano",
+	"anthropic":  "claude-sonnet-4-20250514",
+	"google":     "gemini-2.5-flash",
+}
+
+func TestAll(prompt string) {
+	for providerName, modelName := range providerConfig {
+		fmt.Println("---")
+		client, err := apc.New(providerName)
+		if err != nil {
+			fmt.Printf("\n%v\n", err)
+			fmt.Println("---")
+			continue
+		}
+		fmt.Printf("%s —> ", providerName)
+		respBytes, err := client.SendChatCompletionRequest(modelName, "user", prompt)
+		if err != nil {
+			fmt.Printf("failed:\n\n")
+			fmt.Printf("\n%v\n", err)
+			fmt.Println("---")
+			continue
+		}
+		answer, err := client.ExtractAnswerFromChatCompletionResponse(respBytes)
+		if err != nil {
+			fmt.Printf("failed:\n\n")
+			fmt.Printf("\n%v\n", err)
+			fmt.Println("---")
+			continue
+		}
+		fmt.Printf("%s\n\n", answer)
+		fmt.Println("---")
+	}
+}
+
 func main() {
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
-	// x, err := http.New().Get(ctx, "https://postman-echo.com/get", nil)
-	// if err != nil {
-	// 	fmt.Printf("%v\n", err)
-	// }
-	// fmt.Println(string(x))
-
-	var err error
-	orClient, err := apc.New("openrouter")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-	groqClient, err := apc.New("groq")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-	cerebrasClient, err := apc.New("cerebras")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-	openaiClient, err := apc.New("openai")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-	googleClient, err := apc.New("google")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	anthropicClient, err := apc.New("anthropic")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	var respBytes []byte
-	var answer string
-	respBytes, err = orClient.SendChatCompletionRequest("moonshotai/kimi-k2", "user", "show CWD")
-	if err != nil {
-		fmt.Printf("Failed to send chat completion request: %v\n", err)
-		return
-	}
-	answer, err = orClient.ExtractAnswerFromChatCompletionResponse(respBytes)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%s : %s\n\n", orClient.Name(), answer)
-
-	respBytes, err = groqClient.SendChatCompletionRequest("openai/gpt-oss-120b", "user", "show CWD")
-	if err != nil {
-		fmt.Printf("Failed to send chat completion request: %v\n", err)
-		return
-	}
-	answer, err = groqClient.ExtractAnswerFromChatCompletionResponse(respBytes)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%s : %s\n\n", groqClient.Name(), answer)
-
-	respBytes, err = cerebrasClient.SendChatCompletionRequest("gpt-oss-120b", "user", "show CWD")
-	if err != nil {
-		fmt.Printf("Failed to send chat completion request: %v\n", err)
-		return
-	}
-	answer, err = cerebrasClient.ExtractAnswerFromChatCompletionResponse(respBytes)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%s : %s\n\n", cerebrasClient.Name(), answer)
-
-	respBytes, err = openaiClient.SendChatCompletionRequest("gpt-5", "user", "show CWD")
-	if err != nil {
-		fmt.Printf("Failed to send chat completion request: %v\n", err)
-		return
-	}
-	answer, err = openaiClient.ExtractAnswerFromChatCompletionResponse(respBytes)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%s : %s\n\n", openaiClient.Name(), answer)
-
-	respBytes, err = googleClient.SendChatCompletionRequest("gemini-2.5-flash", "user", "show CWD")
-	if err != nil {
-		fmt.Printf("Failed to send chat completion request: %v\n", err)
-		return
-	}
-	answer, err = googleClient.ExtractAnswerFromChatCompletionResponse(respBytes)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%s : %s\n\n", googleClient.Name(), answer)
-
-	respBytes, err = anthropicClient.SendChatCompletionRequest("claude-opus-4-1-20250805", "user", "show CWD")
-	if err != nil {
-		fmt.Printf("Failed to send chat completion request: %v\n", err)
-		return
-	}
-	answer, err = anthropicClient.ExtractAnswerFromChatCompletionResponse(respBytes)
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%s : %s\n\n", anthropicClient.Name(), answer)
+	TestAll("which module contains dynamic function execution implementation in the current Go project in CWD. Be concise, return module filename only")
 }
